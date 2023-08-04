2e6b2ea22a3a6a950064f287057fa6f50766ee00
harshil chudasama <harshilc71@gmail.com>
2023-08-05 00:52:37 +0530

    db push

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+import { Icons } from "./Icons";
+import { buttonVariants } from "./ui/Button";
+
+function Navbar() {
+	return (
+		<div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
+			<div className="container max-w-7x1 h-full mx-auto flex items-center justify-between gap-2">
+				{/* logo */}
+				<Link href="/" className="flex gap-2 items-center">
+					<Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
+					<p className="hidden textzinc-700 text-sm font-medium md:block">
+						Breadit
+					</p>
+				</Link>
+				{/* search bar  */}
+				<Link href="/sign-in" className={buttonVariants()}>
+					Sign In
+				</Link>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar;
